732282a8bd894ef1819bb5c979991ece9c61b881
MingXuMaOu <92174693+MingXuMaOu@users.noreply.github.com>
2023-01-15 22:47:54 +0800

    Initial Commit

---
+++ FruitMachine/FruitMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMachineApp.swift
+//  FruitMachine
+//
+//  Created by 刘铭 on 2023/1/15.
+//
+
+import SwiftUI
+
+@main
+struct FruitMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
